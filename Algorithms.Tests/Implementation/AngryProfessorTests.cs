3bed69e2a6042fac4b1f389525a6ebbc50907a6d
Mark <mark@dev.haus>
2016-06-20 15:47:22 +0900

    Angry Professor done

---
+++ Algorithms.Tests/Implementation/AngryProfessorTests.cs
@@ -0,0 +1,49 @@
+﻿using Algorithms.Implementation;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Algorithms.Tests.Implementation
+{
+    public class AngryProfessorTests
+    {
+        private AngryProfessor _challenge;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            _challenge = new AngryProfessor();
+        }
+
+
+        [Test]
+        public void IsCancelled_GivenHRCaseOne_Cancelled()
+        {
+            // Arrange
+            int numStudentsRequired = 3;
+            int[] arrivalTimes = {-1, -3, 4, 2};
+
+            // Act
+            var isCancelled = _challenge.IsCancelled(numStudentsRequired, arrivalTimes);
+
+
+            // Assert
+            isCancelled.ShouldBe("YES");
+        }
+
+        [Test]
+        public void IsCancelled_GivenHRCaseTwo_NotCancelled()
+        {
+            // Arrange
+            int numStudentsRequired = 2;
+            int[] arrivalTimes = { 0, -1, 2, 1 };
+
+            // Act
+            var isCancelled = _challenge.IsCancelled(numStudentsRequired, arrivalTimes);
+
+
+            // Assert
+            isCancelled.ShouldBe("NO");
+        }
+
+    }
+}
---
+++ Algorithms/Implementation/AngryProfessor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Implementation
+{
+    // https://www.hackerrank.com/challenges/angry-professor
+
+    public class AngryProfessor
+    {
+        private const string TRUE = "YES";
+        private const string FALSE = "NO";
+
+
+        // Code in the Hacker Rank page
+        //static void Main(String[] args)
+        //{
+        //    int t = Convert.ToInt32(Console.ReadLine());
+        //
+        //    for (int a0 = 0; a0 < t; a0++)
+        //    {
+        //        string[] tokens_n = Console.ReadLine().Split(' ');
+        //        int k = Convert.ToInt32(tokens_n[1]);
+        //        string[] a_temp = Console.ReadLine().Split(' ');
+        //        int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+        //
+        //        Console.WriteLine(IsCancelled(k, a));
+        //    }
+        //
+        //}
+
+
+
+
+
+        public string IsCancelled(int numberOfOnTimeStudentsRequired, IEnumerable<int> arrivalTimes)
+        {
+            var numberOfStudentsOnTime = arrivalTimes.Count(WasOnTime);
+
+
+            if (numberOfOnTimeStudentsRequired > numberOfStudentsOnTime)
+                return TRUE;
+            else
+                return FALSE;
+        }
+
+        private bool WasOnTime(int timeOfArrival)
+        {
+            return timeOfArrival <= 0;
+        }
+
+
+    }
+}
