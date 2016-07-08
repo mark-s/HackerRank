f4df4aa8459f846aeea5c9503ba25d72748fd34e
Mark <mark@dev.haus>
2016-07-08 17:58:33 +0900

    Added first go of the Kangaroo

---
+++ Algorithms.Tests/Implementation/KangarooTests.cs
@@ -0,0 +1,86 @@
+﻿using Algorithms.Implementation;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Algorithms.Tests.Implementation
+{
+
+    [TestFixture]
+    public class KangarooTests
+    {
+        private Kangaroo _challenge;
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+
+            _challenge = new Kangaroo();
+        }
+
+        [Test]
+        public void GivenHAackerRankInput1_GivesYES()
+        {
+            // Arrange
+            var x1 = 0;
+            var v1 = 3;
+            var x2 = 4;
+            var v2 = 2;
+
+            // Act
+            var result = _challenge.HackerRankInputHandler(x1, v1, x2, v2);
+
+            // Assert
+            result.ShouldBe("YES");
+        }
+
+        [Test]
+        public void GivenHAackerRankInput2_GivesNO()
+        {
+            // Arrange
+            var x1 = 0;
+            var v1 = 2;
+            var x2 = 5;
+            var v2 = 3;
+
+            // Act
+            var result = _challenge.HackerRankInputHandler(x1, v1, x2, v2);
+
+            // Assert
+            result.ShouldBe("NO");
+        }
+
+        [Test]
+        public void GivenHAackerRankInput10_GivesNO()
+        {
+            // 43 2 70 2
+            // Arrange
+            var x1 = 43;
+            var v1 = 2;
+            var x2 = 70;
+            var v2 = 2;
+
+            // Act
+            var result = _challenge.HackerRankInputHandler(x1, v1, x2, v2);
+
+            // Assert
+            result.ShouldBe("NO");
+        }
+
+        [Test]
+        public void GivenHAackerRankInput21_GivesYES()
+        {
+            // Arrange
+            var x1 = 4602;
+            var v1 = 8519;
+            var x2 = 7585;
+            var v2 = 8362;
+
+            // Act
+            var result = _challenge.HackerRankInputHandler(x1, v1, x2, v2);
+
+            // Assert
+            result.ShouldBe("YES");
+        }
+
+    }
+}
---
+++ Algorithms/Implementation/kangaroo.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms.Implementation
+{
+    // https://www.hackerrank.com/challenges/kangaroo
+
+    internal class KangarooStats
+    {
+        public int JumpDistance { get; set; }
+        public int CurrentLocation { get; set; }
+    }
+
+
+    public class Kangaroo
+    {
+
+        public string HackerRankInputHandler(int x1, int v1, int x2, int v2)
+        {
+            var kangaroo1 = new KangarooStats { CurrentLocation = x1, JumpDistance = v1 };
+            var kangaroo2 = new KangarooStats { CurrentLocation = x2, JumpDistance = v2 };
+
+
+            if (kangaroo1.CurrentLocation < kangaroo2.CurrentLocation && kangaroo1.JumpDistance <= kangaroo2.JumpDistance)
+                return "NO";
+
+            return (kangaroo1.CurrentLocation - kangaroo2.CurrentLocation) % (kangaroo1.JumpDistance - kangaroo2.JumpDistance)
+                        == 0 ? "YES" : "NO";
+        }
+
+
+    }
+}
